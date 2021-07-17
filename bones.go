abce21a23e6de7c330b0d450837dbccf40979a48
Patrick Thomison <pthomison@peak6.com>
2021-07-17 15:14:49 -0700

    bump

--- bones.go
+++ bones.go
@@ -8,6 +8,16 @@ func bones() MessageBoard {
 				Message: "Hello World This Is A Test",
 				Date:    "12-12-12",
 			},
+			Message{
+				Author:  "John Doe",
+				Message: "Hello World This Is B Test",
+				Date:    "12-12-12",
+			},
+			Message{
+				Author:  "John Doe",
+				Message: "Hello World This Is C Test",
+				Date:    "12-12-12",
+			},
 		},
 	}
 }
