b265db37949046d5e977b85ce82a83facdcde34d
Patrick Thomison <pthomison@peak6.com>
2021-07-18 09:45:47 -0700

    form test

--- message.go
+++ message.go
@@ -1,14 +1,44 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
 type Message struct {
 	Author  string
 	Message string
 	Date    string
 }
 
-// func messageBoardHandler(w http.ResponseWriter, r *http.Request) {
-// 	t, err := template.ParseFS(mbHtml, "ui/*")
-// 	utils.Check(err)
-// 	err = t.Execute(w, nil)
-// 	utils.Check(err)
-// }
+func messageHandler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		errorHandler(w, r, http.StatusNotFound)
+	case http.MethodPost:
+		messagePostHandler(w, r)
+	case http.MethodPut:
+		errorHandler(w, r, http.StatusNotFound)
+	case http.MethodDelete:
+		errorHandler(w, r, http.StatusNotFound)
+	default:
+		errorHandler(w, r, http.StatusNotFound)
+	}
+}
+
+func messagePostHandler(w http.ResponseWriter, r *http.Request) {
+	for k, v := range r.Form {
+		fmt.Println("key:", k)
+		fmt.Println("val:", strings.Join(v, ""))
+	}
+	fmt.Fprintf(w, "Thanks for the data!") // write data to response
+}
+
+// https://stackoverflow.com/questions/9996767/showing-custom-404-error-page-with-standard-http-package
+func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
+	w.WriteHeader(status)
+	if status == http.StatusNotFound {
+		fmt.Fprint(w, "custom 404")
+	}
+}
--- server.go
+++ server.go
@@ -1,25 +1,16 @@
 package main
 
 import (
-	"fmt"
-	"html"
 	"log"
 	"net/http"
 )
-
-// //go:embed ui/*.css
-// var css embed.FS
 
 func RunServer() {
 	http.HandleFunc("/", messageBoardHandler)
 
 	http.HandleFunc("/style.css", cssHandler)
 
-	// http.Handle("/css/", http.FileServer(http.FS(css)))
-
-	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
+	http.HandleFunc("/message", messageHandler)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
